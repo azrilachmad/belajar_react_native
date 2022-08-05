79f30470025fe82eaefcac33b39cbe99212344d5
azrilachmad <azrilachmad4@gmail.com>
2022-08-05 14:59:14 +0700

    Belajar React Native

--- App.js
+++ App.js
@@ -1,110 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
+  StyleSheet,
+  Button,
+  Alert,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.layouts}>
+      <CardComponent />
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const CardComponent = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> Hello World
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text style={{marginBottom: 10, fontSize: 24, color: 'black'}}>
+        List Items :{' '}
+      </Text>
+      <View style={styles.card}>
+        <CardDetails />
+      </View>
+    </View>
+  );
+};
+
+const CardDetails = () => {
+  return (
+    <View>
+      {/* Image */}
+      <Image
+        source={require('./src/images/macbook.jpg')}
+        style={styles.image}
+      />
+      {/* Item Detail */}
+      <Text style={styles.titleText}>New Macbook M2 Pro</Text>
+      {/* Item Price */}
+      <Text style={styles.priceText}>Rp. 25.000.000</Text>
+      {/* Item Location */}
+      <Text style={styles.cityText}>Bandung</Text>
+      {/* Button */}
+      <TouchableOpacity
+        onPress={() => Alert.alert('Button pressed')}
+        style={styles.buttonLayout}>
+        <Text style={styles.ButtonText}>Buy Now</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
+  layouts: {
+    paddingVertical: 24,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
+  card: {
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    width: 170,
+    height: 250,
+    backgroundColor: '#DFDFDF',
+    borderRadius: 18,
+  },
+  image: {
+    marginBottom: 8,
+    width: '100%',
+    height: '60%',
+    borderRadius: 8,
+  },
+  titleText: {
+    color: 'black',
+    fontSize: 13,
     fontWeight: '600',
   },
-  sectionDescription: {
+  priceText: {
     marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+    color: 'orange',
+    fontWeight: '500',
+    fontSize: 11,
   },
-  highlight: {
-    fontWeight: '700',
+  cityText: {
+    color: 'gray',
+    fontWeight: '500',
+    fontSize: 9,
+  },
+  buttonLayout: {
+    marginTop: 8,
+    width: '100%',
+    height: 20,
+    backgroundColor: '#84BC9C',
+    alignItems: 'center',
+    borderRadius: 6,
+  },
+  ButtonText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
 
